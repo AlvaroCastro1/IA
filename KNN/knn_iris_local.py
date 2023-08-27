258fd5fec62258ede1b4ca66b9e9b3c1a90d913f
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-08-26 21:04:10 -0600

    datos con nueva clase iris

---
+++ KNN/knn_iris_local.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.datasets import load_iris                  #Carga BD iris
+from sklearn.model_selection import train_test_split      #Dividir en entrenamiento y pruebas
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# este archivo ya contiene los nuevos datos
+archivo = './KNN/Iris_DB.csv'
+
+
+
+
+dataFrame = pd.read_csv(archivo)
+
+print(dataFrame)
+print(type(dataFrame))
+
+x_data = dataFrame[dataFrame.columns[:-1]].to_numpy()
+y_data = dataFrame[dataFrame.columns[-1]].to_numpy()
+
+print(x_data)
+print(type(x_data))
+
+print(y_data)
+print(type(y_data))
+
+              
+#train_test_split   Función para dividir los datos
+#Regresa grupos entreamiento y pruebas, recibe datos y clasificación
+x_entrenar, x_test, y_entrenar, y_test = train_test_split(x_data, y_data )
+
+print(x_entrenar.shape)     #Grupo entrenamiento
+print(y_entrenar.shape)     #Grupo entrenamiento clase
+
+##CLASIFICADOR
+knn = KNeighborsClassifier( n_neighbors=10 )
+
+knn.fit(x_entrenar, y_entrenar)             #Entrenamiento
+
+aprend = knn.score(x_test, y_test)          #Pruebas (Aprendizaje)
+print('{:.5f}'.format(aprend) )
+
+clasif = knn.predict([[3.5, 1.5, 2.0, 0.8]])    # MI Nuevo patrón
+print(clasif)
+
