f93351d9fa92043a99294901e90aeb9728ef00c4
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-09-01 09:24:49 -0600

    graficas 3D con jmathplot listo

--- KNN_3d/src/main/java/com/mycompany/knn_3d/KNN_3d.java
+++ KNN_3d/src/main/java/com/mycompany/knn_3d/KNN_3d.java
@@ -1,27 +1,28 @@
 package com.mycompany.knn_3d;
 
+import java.awt.Color;
 import javax.swing.JFrame;
 import org.math.plot.*;
 
-/**
- *
- * @author Alvaro
- */
 public class KNN_3d {
 
     static double[] x = {1.5, 2.7, 3.2, 4.9, 5.3};
     static double[] y = {1.5, 2.7, 3.2, 4.9, 5.3};
+    static double[] z = {1.0, 2.0, 3.0, 4.0, 5.0}; // Cambiado a un arreglo unidimensional
 
     public static void main(String[] args) {
         // create your PlotPanel (you can use it as a JPanel)
-        Plot2DPanel plot = new Plot2DPanel();
+        Plot3DPanel plot = new Plot3DPanel();
 
-        // add a line plot to the PlotPanel
-        plot.addLinePlot("my plot", x, y);
+        // add a scatter plot to the PlotPanel (points in 3D)
+        plot.addScatterPlot("ejemplo", Color.red, x, y, z);
 
         // put the PlotPanel in a JFrame, as a JPanel
-        JFrame frame = new JFrame("a plot panel");
+        JFrame frame = new JFrame("Plot Panel");
         frame.setContentPane(plot);
         frame.setVisible(true);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
+
